39e1264208edfc1a4fc52b3e344b993e10d98916
vinicius-g-alves <vinicius.garcia.alves10@gmail.com>
2023-06-23 00:43:03 -0300

    #2 - update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'
-import { ConfigProvider } from 'antd';
-import ptBR from 'antd/lib/locale/pt_BR';
-// import 'antd/dist/antd.css';/
-import 'antd/dist/antd.min.css'
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ConfigProvider locale={ptBR}>
-      <App />
-    </ConfigProvider>
+    <App />
   </React.StrictMode>
 );
