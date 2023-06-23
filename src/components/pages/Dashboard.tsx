fbdbef023c93642d3a1155ae72e798a567189e25
vinicius-g-alves <vinicius.garcia.alves10@gmail.com>
2023-06-23 00:43:20 -0300

    #2 - create dashboard component

---
+++ src/components/pages/Dashboard.tsx
@@ -0,0 +1,170 @@
+import { Line } from "@ant-design/charts";
+import { Pie, measureTextWidth } from "@ant-design/plots";
+import { Card, Col, Layout, Row } from "antd";
+
+const Dashboard = () => {
+  const fisrtCardData = [
+    { year: "1991", value: 3 },
+    { year: "1992", value: 4 },
+    { year: "1993", value: 3.5 },
+    { year: "1994", value: 5 },
+    { year: "1995", value: 4.9 },
+    { year: "1996", value: 6 },
+    { year: "1997", value: 7 },
+    { year: "1998", value: 9 },
+    { year: "1999", value: 13 },
+  ];
+  const firstCardConfig = {
+    data: fisrtCardData,
+    height: 400,
+    xField: "year",
+    yField: "value",
+    point: {
+      size: 5,
+      shape: "diamond",
+    },
+  };
+
+  function renderStatistic(containerWidth, text, style) {
+    const { width: textWidth, height: textHeight } = measureTextWidth(
+      text,
+      style
+    );
+    const R = containerWidth / 2; // r^2 = (w / 2)^2 + (h - offsetY)^2
+
+    let scale = 1;
+
+    if (containerWidth < textWidth) {
+      scale = Math.min(
+        Math.sqrt(
+          Math.abs(
+            Math.pow(R, 2) /
+              (Math.pow(textWidth / 2, 2) + Math.pow(textHeight, 2))
+          )
+        ),
+        1
+      );
+    }
+
+    const textStyleStr = `width:${containerWidth}px;`;
+    return `<div style="${textStyleStr};font-size:${scale}em;line-height:${
+      scale < 1 ? 1 : "inherit"
+    };">${text}</div>`;
+  }
+  const data = [
+    {
+      type: "分类一",
+      value: 27,
+    },
+    {
+      type: "分类二",
+      value: 25,
+    },
+    {
+      type: "分类三",
+      value: 18,
+    },
+    {
+      type: "分类四",
+      value: 15,
+    },
+    {
+      type: "分类五",
+      value: 10,
+    },
+    {
+      type: "其他",
+      value: 5,
+    },
+  ];
+  const config = {
+    appendPadding: 10,
+    data,
+    angleField: "value",
+    colorField: "type",
+    radius: 1,
+    innerRadius: 0.64,
+    meta: {
+      value: {
+        formatter: (v) => `${v} ¥`,
+      },
+    },
+    label: {
+      type: "inner",
+      offset: "-50%",
+      style: {
+        textAlign: "center",
+      },
+      autoRotate: false,
+      content: "{value}",
+    },
+    statistic: {
+      title: {
+        offsetY: -4,
+        customHtml: (container, view, datum) => {
+          const { width, height } = container.getBoundingClientRect();
+          const d = Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2));
+          const text = datum ? datum.type : "总计";
+          return renderStatistic(d, text, {
+            fontSize: 28,
+          });
+        },
+      },
+      content: {
+        offsetY: 4,
+        style: {
+          fontSize: "32px",
+        },
+        customHtml: (container, view, datum, data) => {
+          const { width } = container.getBoundingClientRect();
+          const text = datum
+            ? `¥ ${datum.value}`
+            : `¥ ${data.reduce((r, d) => r + d.value, 0)}`;
+          return renderStatistic(width, text, {
+            fontSize: 32,
+          });
+        },
+      },
+    },
+    // 添加 中心统计文本 交互
+    interactions: [
+      {
+        type: "element-selected",
+      },
+      {
+        type: "element-active",
+      },
+      {
+        type: "pie-statistic-active",
+      },
+    ],
+  };
+
+  return (
+    <Layout>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Card>
+            <Line {...firstCardConfig} />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card>
+            <Pie {...config} />
+          </Card>
+        </Col>
+      </Row>
+
+      <Row gutter={24} style={{ marginTop: 24 }}>
+        <Col span={12}>
+          <Card>card1</Card>
+        </Col>
+        <Col span={12}>
+          <Card>card1</Card>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
+
+export default Dashboard;
